330247b058ed4c9152d55bd6192d5195d8dd7ad2
Kelvin Newman <kelvin@cheshiresheds.co.uk>
2026-01-20 16:42:00 +0000

    Add icons to nav bar buttons and increase opacity for better visibility

--- apps/web/src/components/Layout.tsx
+++ apps/web/src/components/Layout.tsx
@@ -25,15 +25,44 @@ export default function Layout() {
           <img src="/logo-horizontal.png" alt="My Hive" className="layout-logo" />
         </div>
         <nav className="layout-nav">
-          <Link to="/">Dashboard</Link>
-          <Link to="/apiaries">Apiaries</Link>
-          <Link to="/map">Map</Link>
-          <Link to="/queens">Queens</Link>
-          <Link to="/shop">Shop</Link>
-          <Link to="/honey">Honey</Link>
-          <Link to="/pests">Pests</Link>
-          <Link to="/maintenance">Maintenance</Link>
-          {user?.role === 'admin' && <Link to="/admin">Admin</Link>}
+          <Link to="/">
+            <img src="/hive-icon.png" alt="" className="icon-small" />
+            Dashboard
+          </Link>
+          <Link to="/apiaries">
+            <img src="/apiary-icon.png" alt="" className="icon-small" />
+            Apiaries
+          </Link>
+          <Link to="/map">
+            <img src="/map-icon.png" alt="" className="icon-small" />
+            Map
+          </Link>
+          <Link to="/queens">
+            <img src="/queen-icon.png" alt="" className="icon-small" />
+            Queens
+          </Link>
+          <Link to="/shop">
+            <img src="/resources-icon.png" alt="" className="icon-small" />
+            Shop
+          </Link>
+          <Link to="/honey">
+            <img src="/bee-icon.png" alt="" className="icon-small" />
+            Honey
+          </Link>
+          <Link to="/pests">
+            <img src="/inspection-icon.png" alt="" className="icon-small" />
+            Pests
+          </Link>
+          <Link to="/maintenance">
+            <img src="/weather-icon.png" alt="" className="icon-small" />
+            Maintenance
+          </Link>
+          {user?.role === 'admin' && (
+            <Link to="/admin">
+              <img src="/profile-icon.png" alt="" className="icon-small" />
+              Admin
+            </Link>
+          )}
           <button onClick={handleLogout} className="btn-link">
             <img src="/profile-icon.png" alt="" className="icon-small" />
             {user?.name}
